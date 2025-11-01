a2065d202a586ee4590e91208a3880f792156c16
James Koch <jvkoch0@gmail.com>
2025-11-01 11:03:45 -0700

    removing references to the game

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
-        training: resolve(__dirname, 'training.html'),
-        game: resolve(__dirname, 'game.html')
+        training: resolve(__dirname, 'training.html')
       }
     }
   },
